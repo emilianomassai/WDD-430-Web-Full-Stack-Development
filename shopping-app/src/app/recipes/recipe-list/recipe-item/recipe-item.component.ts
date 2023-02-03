9cb9983eb57f3c00bfeb8ca55faaee3bcbc7c35d
emilianomassai <46691294+emilianomassai@users.noreply.github.com>
2023-02-03 10:14:55 +0000

    Starting W6 Tutorials

--- shopping-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ shopping-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,21 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
- 
+import { RecipeService } from '../../recipe.service';
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-@Input() recipe: Recipe;
-@Output() recipeSelected = new EventEmitter<void>();
-  constructor() { }
+  @Input() recipe: Recipe;
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
 
-onSelected(){
-this.recipeSelected.emit();
-}
+  onSelected() {
+    this.recipeService.recipeSelected.emit(this.recipe);
+  }
 
 }
